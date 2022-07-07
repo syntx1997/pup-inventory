049e0e62e3d3a618ca6d08d92c111b2672a44169
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-07-07 12:39:51 +0800

    Updates: Supplies Functions and Migration

--- resources/views/pages/dashboard/admin/supplies.blade.php
+++ resources/views/pages/dashboard/admin/supplies.blade.php
@@ -1,3 +1,28 @@
 <x-main-template :title="$title" :fragment="$fragment" :dashboardLink="$dashboardLink">
     @include('partials._page-title')
+
+    <div class="text-end mb-4">
+        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#add-supply-modal">
+            <i class="uil-plus"></i> Add Supply Item
+        </button>
+    </div>
+
+    <div class="table-responsive">
+        <table id="supplies-table" class="table bg-white" style="width: 100%">
+            <thead class="bg-light">
+            <tr>
+                <th class="text-center">Category</th>
+                <th class="text-center">Supply</th>
+                <th class="text-center">Stock</th>
+                <th class="text-center">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td class="text-center" colspan="3">no data</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
 </x-main-template>
